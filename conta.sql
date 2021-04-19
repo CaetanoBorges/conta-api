16fdad46be5f11407704f78d2c818d239b16c5c4
Caetano Borges <cbcaetanoborges@gmail.com>
2021-04-19 13:34:25 +0100

    ----

---
+++ conta.sql
@@ -0,0 +1,76 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Tempo de geração: 19-Abr-2021 às 14:33
+-- Versão do servidor: 10.4.10-MariaDB
+-- versão do PHP: 7.1.33
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `conta`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `conta`
+--
+
+CREATE TABLE `conta` (
+  `id` int(11) NOT NULL,
+  `chave` text NOT NULL,
+  `nome` varchar(50) NOT NULL,
+  `apelido` varchar(50) NOT NULL,
+  `genero` varchar(2) NOT NULL,
+  `dia_nascimento` int(2) NOT NULL,
+  `mes_nascimento` int(2) NOT NULL,
+  `ano_nascimento` int(4) NOT NULL,
+  `telefone` varchar(15) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `palavra_passe` text NOT NULL,
+  `dia_entrada` int(2) NOT NULL,
+  `mes_entrada` int(2) NOT NULL,
+  `ano_entrada` int(4) NOT NULL,
+  `codigo_renova` int(6) NOT NULL,
+  `pais` varchar(50) NOT NULL,
+  `provincia` varchar(50) NOT NULL,
+  `municipio` varchar(50) NOT NULL,
+  `bairro_rua` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices para tabela `conta`
+--
+ALTER TABLE `conta`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `conta`
+--
+ALTER TABLE `conta`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
