67a40fc320d2077af95ce7a06ef67daf6ea2166d
Caetano Borges <cbcaetanoborges@gmail.com>
2021-12-17 14:38:13 +0100

    show

--- classes/cadastra.class.php
+++ classes/cadastra.class.php
@@ -39,7 +39,7 @@ class Cadastrar
 
     public function novoUser($array)
     {
-        $stmt = $this->db->prepare('INSERT INTO conta (chave, nome, apelido, genero, dia_nascimento, mes_nascimento, ano_nascimento, telefone, email, palavra_passe, dia_entrada, mes_entrada, ano_entrada, codigo_renova, pais, provincia, municipio, bairro_rua) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)');
+        $stmt = $this->db->prepare('INSERT INTO conta (chave, nome, apelido, genero, dia_nascimento, mes_nascimento, ano_nascimento, telefone, email, palavra_passe, dia_entrada, mes_entrada, ano_entrada, codigo_renova, pais, provincia, municipio, bairro_rua, foto) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)');
         $stmt -> bindValue(1, chaveDB());
         $stmt -> bindValue(2, $array['nome']);
         $stmt -> bindValue(3, $array['apelido']);
@@ -54,10 +54,11 @@ class Cadastrar
         $stmt -> bindValue(12, date('m'));
         $stmt -> bindValue(13, date('Y'));
         $stmt -> bindValue(14, 0);
-        $stmt -> bindValue(15, "");
+        $stmt -> bindValue(15, "Angola");
         $stmt -> bindValue(16, "");
         $stmt -> bindValue(17, "");
         $stmt -> bindValue(18, "");
+        $stmt -> bindValue(19, "default.png");
         
         
         if ($stmt ->execute()) {
